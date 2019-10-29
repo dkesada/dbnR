0d4e0acaf7d282399c9ec519202e032cec87be27
dkesada <dkesada@gmail.com>
2019-10-29 18:02:41 +0100

    Calculate the mu vector with rcpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,20 +5,21 @@
 
 using namespace Rcpp;
 
-// sum_vec
-int sum_vec(NumericVector x);
-RcppExport SEXP _dbnR_sum_vec(SEXP xSEXP) {
+// calc_mu
+std::map<std::string, float> calc_mu(Rcpp::List fit, std::vector<std::string> order);
+RcppExport SEXP _dbnR_calc_mu(SEXP fitSEXP, SEXP orderSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(sum_vec(x));
+    Rcpp::traits::input_parameter< Rcpp::List >::type fit(fitSEXP);
+    Rcpp::traits::input_parameter< std::vector<std::string> >::type order(orderSEXP);
+    rcpp_result_gen = Rcpp::wrap(calc_mu(fit, order));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_dbnR_sum_vec", (DL_FUNC) &_dbnR_sum_vec, 1},
+    {"_dbnR_calc_mu", (DL_FUNC) &_dbnR_calc_mu, 2},
     {NULL, NULL, 0}
 };
 
--- src/gauss_transform.cpp
+++ src/gauss_transform.cpp
@@ -1,15 +1,31 @@
-
 #include <Rcpp.h>
 using namespace Rcpp;
+#include <map>
 
-//' Test function
-//' @param x a vector of numbers
+//' Calculate the mu vector of means of a Gaussian linear network
+//' 
+//' @param fit a bn.fit object
+//' @param order a topological ordering of the nodes
+//' @return the numeric named vector of means
 //' @export
 // [[Rcpp::export]]
-int sum_vec(NumericVector x) {
-  int acc = 0;
-  for(int i = 0; i < x.length(); i++)
-    acc += x[i];
-
-  return acc;
+std::map<std::string, float> calc_mu(Rcpp::List fit, std::vector<std::string> order){
+  std::map<std::string, float> mu;
+  Rcpp::List node;
+  std::vector<std::string> parents;
+  std::vector<float> coefs;
+  
+  for(unsigned int i = 0; i < order.size(); i++){
+    // Extract the relevant elements from the lists
+    node = fit[order[i]];
+    parents = node["parents"];
+    coefs = node["coefficients"];
+    
+    mu[order[i]] = coefs[0];
+    for(unsigned int j = 1; j < coefs.size(); j++){
+      mu[order[i]] += coefs[j] * mu[parents[j-1]];
+    }
+  }
+  
+  return mu;
 }
