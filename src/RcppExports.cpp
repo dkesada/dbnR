349864d2935d8505a8886a9bb93cc1cbd550a160
dkesada <dkesada@gmail.com>
2019-10-30 15:15:13 +0100

    building on linux

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -29,3 +29,14 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+
+static const R_CallMethodDef CallEntries[] = {
+    {"_dbnR_calc_mu_cpp", (DL_FUNC) &_dbnR_calc_mu_cpp, 2},
+    {"_dbnR_calc_sigma_cpp", (DL_FUNC) &_dbnR_calc_sigma_cpp, 2},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_dbnR(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
