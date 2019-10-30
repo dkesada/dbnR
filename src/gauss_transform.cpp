400b512ed75eca4f8bff61c7308afb382801f25e
dkesada <dkesada@gmail.com>
2019-10-30 16:34:24 +0100

    doesnt build in linux

--- src/gauss_transform.cpp
+++ src/gauss_transform.cpp
@@ -17,7 +17,7 @@ std::map<std::string, float> calc_mu_cpp(Rcpp::List &fit, std::vector<std::strin
   
   for(unsigned int i = 0; i < order.size(); i++){
     // Extract the relevant elements from the lists
-    node = fit[order[i]];
+    node = fit[order[i]]; // Doesn't work on linux when building
     parents = node["parents"];
     coefs = node["coefficients"];
     
