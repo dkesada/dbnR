9325f8e0e5f096d87274801c03f0be0683090994
dkesada <dkesada@gmail.com>
2019-10-31 14:19:51 +0100

    working main flow test

---
+++ tests/testthat/test-forecast_dbn.R
@@ -0,0 +1,16 @@
+test_that("working_forecast_dbn", {
+  data(motor)
+  dt <- motor
+  size = 3
+  dt_train <- dt[1:500]
+  dt_test <- dt[501:100]
+  f_dt_train <- fold_dt(dt_train, size)
+  f_dt_test <- fold_dt(dt_test, size)
+  
+  net <- learn_dbn_struc(dt_train, size = size)
+  fit <- fit_dbn_params(net, f_dt_train)
+  res_fore <- forecast_ts(f_dt_test, fit, size, "pm_t_0", rep = 2,
+                          ini = 1, len = 50, print_res = F, plot_res = F)
+  
+  expect_equal(1, 1)
+})
