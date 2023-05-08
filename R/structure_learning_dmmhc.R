bc8ad2b6725233403b26db766fffa882c931d698
Prashasti Agarwal <prashasti19075@iiitd.ac.in>
2023-05-08 19:02:29 +0530

    added boostrapping to dmmhc method. Need to add checks for the boostrap params.

--- R/structure_learning_dmmhc.R
+++ R/structure_learning_dmmhc.R
@@ -71,30 +71,41 @@ merge_nets <- function(net0, netCP1, size, acc = NULL, slice = 1){
 #' @param f_dt previously folded dataset, in case some specific rows have to be removed after the folding
 #' @param blacklist an optional matrix indicating forbidden arcs between nodes
 #' @param intra if TRUE, the intra-slice arcs of the network will be learnt. If FALSE, they will be ignored
-#' @param blacklist_tr an optional matrix indicating forbidden intra-slice arcs between nodes 
+#' @param blacklist_tr an optional matrix indicating forbidden intra-slice arcs between nodes  
+#' @param nboots an optional integer indicating the number of boostrapped networks to be created, -1 for no boostrapping
+#' @param edge_strength an optional numeric indicating the edge strength for the boostrapped networks
+#' @param direction_strength an optional numeric indicating the direction strength for the boostrapped networks 
+#' @param cluster an optional cluster object to be used for parallelization
 #' @param ... additional parameters for \code{\link{rsmax2}} function
 #' @return the structure of the net
 #' @import data.table
 #' @keywords internal
 dmmhc <- function(dt, size = 2, f_dt = NULL, blacklist = NULL, intra = TRUE,
-                  blacklist_tr = NULL, ...){
+                  blacklist_tr = NULL, nboots = 2, edge_strength = 0.51,
+                   direction_strength = 0.51, cluster = NULL,  ...){
   dt_null_check(dt, intra)
   
   if(!is.null(dt)){
     dt <- time_rename(dt)
     if(intra){
       dt_copy <- data.table::copy(dt)
-      net0 <- bnlearn::rsmax2(x = dt_copy, blacklist = blacklist, ...) # Static network
+      if(nboots == -1)
+        net0 <- bnlearn::rsmax2(x = dt_copy, blacklist = blacklist, ...) # Static network
+      else
+        net0 <- bootstrap(data = dt_copy, blacklist = blacklist, nboots = nboots,edge_strength = edge_strength, direction_strength = direction_strength,cluster = cluster ...)
     }
   }
-  
   if(is.null(f_dt))
     f_dt <- fold_dt_rec(dt, names(dt), size)
   
   blacklist <- create_blacklist(names(f_dt), size)
   blacklist <- rbind(blacklist, blacklist_tr)
   
-  net <- bnlearn::rsmax2(x = f_dt, blacklist = blacklist, ...) # Transition network
+  if(nboots == -1)
+    net <- bnlearn::rsmax2(x = f_dt, blacklist = blacklist, ...) # Transition network
+  else
+    net <- bootstrap(data = f_dt, blacklist = blacklist, nboots = nboots, edge_strength = edge_strength, direction_strength = direction_strength, cluster = cluster, ...)
+    
   check_empty_net(net)
   
   if(intra && !warn_empty_net(net0))
@@ -102,4 +113,28 @@ dmmhc <- function(dt, size = 2, f_dt = NULL, blacklist = NULL, intra = TRUE,
   class(net) <- c("dbn", class(net))
   
   return(net)
+}
+
+#' Boostraps a Network and returns the average network
+#' @param data the data.frame or data.table to be used
+#' @param blacklist an optional matrix indicating forbidden arcs between nodes
+#' @param nboots an integer indicating the number of boostrapped networks to be created, -1 for no boostrapping
+#' @param edge_strength an optional numeric indicating the edge strength for the boostrapped networks
+#' @param direction_strength an optional numeric indicating the direction strength for the boostrapped networks
+#' @param cluster an optional cluster object to be used for parallelization
+#' @param algo an optional string indicating the algorithm to be used for learning the network
+#' @param scoring an optional string indicating the scoring function to be used for learning the network
+#' @param ... additional parameters for \code{\link{rsmax2}} function
+#' @return the structure of the net
+#' @import data.table
+#' @keywords internal
+bootstrap <- function(data, blacklist = NULL, nboots, edge_strength = 0.51, direction_strength = 0.51, cluster = NULL, algo = 'rsmax2', scoring = 'bic', ...){
+  bootstapped <- boot.strength(data = data, R = nboots,
+                              m = nrow(data), algorithm = algo,
+                              algorithm.args=list(blacklist=blacklist,
+                              score = scoring, ...), cluster = cluster)
+  
+  pruned <- bootstapped[bootstapped$strength >= edgeStrength & bootstapped$direction >= directionStrength,]
+  net <- cextend(averaged.network(pruned))
+  return (net)
 }
